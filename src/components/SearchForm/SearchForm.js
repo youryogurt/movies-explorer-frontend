6a070d26ca9bc4a42ce04816485d7d2228201fc6
Janna Gurtovaia <zhanna.gurt@gmail.com>
2023-07-18 10:28:21 +0400

    rewrite search for for mobile amd desc

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import FilterCheckbox from '../FilterCheckbox/FilterCheckbox';
-import { ReactComponent as SearchButtonIcon } from '../../images/search-arrow.svg';
-import search from '../../images/search-icon.svg';
+import React from "react";
+import FilterCheckbox from "../FilterCheckbox/FilterCheckbox";
+import { ReactComponent as SearchButtonIcon } from "../../images/search-arrow.svg";
+import search from "../../images/search-icon.svg";
 
 function SearchForm() {
   return (
     <section className="search-form__section">
-      <img className="search-form__icon" alt="Лупа" src={search} />
-      <form className="search-form">
-        <input
-             type="text"
-             placeholder="Фильм"
-             className="search-input"
-         />
-        <button className="search-form__button">
-           <SearchButtonIcon className="search-button-icon" />
-         </button>
-       </form>
-       <div className="search-form__vertical-line"></div>
-       <FilterCheckbox/>
+      <div className="search-form">
+        <img className="search-form__icon" alt="Лупа" src={search} />
+        <div className="search-form__container">
+          <input type="text" placeholder="Фильм" className="search-input" />
+          <div className="search-form__items">
+            <button type="submit" className="search-form__button">
+              <SearchButtonIcon />
+            </button>
+            <div className="search-form__line"></div>
+            <FilterCheckbox
+              className="search-form__checkbox"
+              text="Короткометражки"
+            />
+          </div>
+
+          <div className="search-form__items_mobile">
+            <button type="submit" className="search-form__button">
+              <SearchButtonIcon />
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="search-form__mobile-checkbox">
+        <FilterCheckbox
+          className="search-form__checkbox"
+          text="Короткометражки"
+        />
+      </div>
     </section>
-  )
+  );
 }
 
 export default SearchForm;
